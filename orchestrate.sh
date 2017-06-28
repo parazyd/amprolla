a0b382bf60dfba2066a795a91fba4ad3b7339f2f
parazyd <parazyd@dyne.org>
2017-06-28 20:09:17 +0200

    be more informational when rsyncing

--- orchestrate.sh
+++ orchestrate.sh
@@ -11,8 +11,12 @@ while true; do
 	ln -snf "$REPO_ROOT"/merged-staging "$REPO_ROOT"/merged
 	# the break call is temporary to catch unhandled exceptions in the testing phase
 	python3 "$AMPROLLA_UPDATE" || break
+	printf "rsyncing volatile to production...\n"
 	rsync --delete -raX "$REPO_ROOT"/merged-volatile/* "$REPO_ROOT"/merged-production
+	printf "done!\n"
 	ln -snf "$REPO_ROOT"/merged-production "$REPO_ROOT"/merged
+	printf "rsyncing volatile to staging...\n"
 	rsync --delete -raX "$REPO_ROOT"/merged-volatile/* "$REPO_ROOT"/merged-staging
+	printf "done!\n"
 	sleep 3600
 done
